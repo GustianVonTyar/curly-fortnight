4ec555bb08ca28978387d53e912304bd8b357518
GustianVonTyar <122793144+GustianVonTyar@users.noreply.github.com>
2023-01-17 21:43:33 +0700

    Add files via upload

---
+++ kopf.h
@@ -0,0 +1,8 @@
+ serg()
+{
+   cout<<"«::::::Gustian_Nouvantyar::::::»"<<endl;
+   cout<<"«:::::::::::::X-PPLG:::::::::::»"<<endl;
+   cout<<"Absen : 13"<<endl;
+   cout<<"Username : papo"<<endl;
+   cout<<"Password : csodaszarvasunknak"<<endl;
+}
